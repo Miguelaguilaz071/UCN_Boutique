ee4eaf2ea1355b403065eeb074ee580e31962cd9
Miguel Ángel <miguelaguilaz180@gmail.com>
2026-01-19 19:59:56 -0600

    Implementación de botones de opciones

--- Capa de presentacion/FrmAdmin.Designer.cs
+++ Capa de presentacion/FrmAdmin.Designer.cs
@@ -35,6 +35,11 @@
             this.btnUsuarios = new System.Windows.Forms.Button();
             this.panelCentral = new System.Windows.Forms.Panel();
             this.uC_Usuario1 = new Capa_de_presentacion.UC_Usuario();
+            this.btnSucursales = new System.Windows.Forms.Button();
+            this.btnInventario = new System.Windows.Forms.Button();
+            this.btnMarcas = new System.Windows.Forms.Button();
+            this.btnRoles = new System.Windows.Forms.Button();
+            this.btnPagos = new System.Windows.Forms.Button();
             this.panelOpciones.SuspendLayout();
             this.panelCentral.SuspendLayout();
             this.SuspendLayout();
@@ -42,6 +47,11 @@
             // panelOpciones
             // 
             this.panelOpciones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
+            this.panelOpciones.Controls.Add(this.btnPagos);
+            this.panelOpciones.Controls.Add(this.btnRoles);
+            this.panelOpciones.Controls.Add(this.btnMarcas);
+            this.panelOpciones.Controls.Add(this.btnInventario);
+            this.panelOpciones.Controls.Add(this.btnSucursales);
             this.panelOpciones.Controls.Add(this.btnVentas);
             this.panelOpciones.Controls.Add(this.btnProductos);
             this.panelOpciones.Controls.Add(this.btnUsuarios);
@@ -56,9 +66,9 @@
             this.btnVentas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.btnVentas.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnVentas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVentas.Location = new System.Drawing.Point(700, 5);
+            this.btnVentas.Location = new System.Drawing.Point(900, 5);
             this.btnVentas.Name = "btnVentas";
-            this.btnVentas.Size = new System.Drawing.Size(150, 30);
+            this.btnVentas.Size = new System.Drawing.Size(120, 30);
             this.btnVentas.TabIndex = 1;
             this.btnVentas.Text = "Ventas";
             this.btnVentas.UseVisualStyleBackColor = false;
@@ -68,9 +78,9 @@
             this.btnProductos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.btnProductos.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnProductos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProductos.Location = new System.Drawing.Point(416, 5);
+            this.btnProductos.Location = new System.Drawing.Point(138, 5);
             this.btnProductos.Name = "btnProductos";
-            this.btnProductos.Size = new System.Drawing.Size(150, 30);
+            this.btnProductos.Size = new System.Drawing.Size(120, 30);
             this.btnProductos.TabIndex = 1;
             this.btnProductos.Text = "Productos";
             this.btnProductos.UseVisualStyleBackColor = false;
@@ -81,9 +91,9 @@
             this.btnUsuarios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.btnUsuarios.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUsuarios.Location = new System.Drawing.Point(133, 5);
+            this.btnUsuarios.Location = new System.Drawing.Point(12, 5);
             this.btnUsuarios.Name = "btnUsuarios";
-            this.btnUsuarios.Size = new System.Drawing.Size(150, 30);
+            this.btnUsuarios.Size = new System.Drawing.Size(120, 30);
             this.btnUsuarios.TabIndex = 0;
             this.btnUsuarios.Text = "Usuarios";
             this.btnUsuarios.UseVisualStyleBackColor = false;
@@ -105,6 +115,66 @@
             this.uC_Usuario1.Name = "uC_Usuario1";
             this.uC_Usuario1.Size = new System.Drawing.Size(1032, 562);
             this.uC_Usuario1.TabIndex = 0;
+            // 
+            // btnSucursales
+            // 
+            this.btnSucursales.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnSucursales.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSucursales.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSucursales.Location = new System.Drawing.Point(264, 5);
+            this.btnSucursales.Name = "btnSucursales";
+            this.btnSucursales.Size = new System.Drawing.Size(120, 30);
+            this.btnSucursales.TabIndex = 2;
+            this.btnSucursales.Text = "Sucursales";
+            this.btnSucursales.UseVisualStyleBackColor = false;
+            // 
+            // btnInventario
+            // 
+            this.btnInventario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnInventario.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInventario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventario.Location = new System.Drawing.Point(390, 5);
+            this.btnInventario.Name = "btnInventario";
+            this.btnInventario.Size = new System.Drawing.Size(120, 30);
+            this.btnInventario.TabIndex = 3;
+            this.btnInventario.Text = "Inventario";
+            this.btnInventario.UseVisualStyleBackColor = false;
+            // 
+            // btnMarcas
+            // 
+            this.btnMarcas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnMarcas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMarcas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMarcas.Location = new System.Drawing.Point(516, 5);
+            this.btnMarcas.Name = "btnMarcas";
+            this.btnMarcas.Size = new System.Drawing.Size(120, 30);
+            this.btnMarcas.TabIndex = 4;
+            this.btnMarcas.Text = "Marcas";
+            this.btnMarcas.UseVisualStyleBackColor = false;
+            // 
+            // btnRoles
+            // 
+            this.btnRoles.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnRoles.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRoles.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoles.Location = new System.Drawing.Point(642, 5);
+            this.btnRoles.Name = "btnRoles";
+            this.btnRoles.Size = new System.Drawing.Size(120, 30);
+            this.btnRoles.TabIndex = 5;
+            this.btnRoles.Text = "Roles";
+            this.btnRoles.UseVisualStyleBackColor = false;
+            // 
+            // btnPagos
+            // 
+            this.btnPagos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnPagos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPagos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPagos.Location = new System.Drawing.Point(768, 5);
+            this.btnPagos.Name = "btnPagos";
+            this.btnPagos.Size = new System.Drawing.Size(120, 30);
+            this.btnPagos.TabIndex = 6;
+            this.btnPagos.Text = "Pagos";
+            this.btnPagos.UseVisualStyleBackColor = false;
             // 
             // FrmAdmin
             // 
@@ -132,5 +202,10 @@
         private System.Windows.Forms.Button btnUsuarios;
         private System.Windows.Forms.Panel panelCentral;
         private UC_Usuario uC_Usuario1;
+        private System.Windows.Forms.Button btnInventario;
+        private System.Windows.Forms.Button btnSucursales;
+        private System.Windows.Forms.Button btnMarcas;
+        private System.Windows.Forms.Button btnRoles;
+        private System.Windows.Forms.Button btnPagos;
     }
 }
